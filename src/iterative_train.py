90b7c4a5f67efe5d708b7414ca7ca6d1e23657ec
Shira <159072972+Shira7z@users.noreply.github.com>
2024-12-20 02:19:10 -0500

    Update iterative_train.py

--- src/iterative_train.py
+++ src/iterative_train.py
@@ -5,10 +5,13 @@ from dataset import HighConfidenceDataset
 from pseudo_labeling import generate_pseudo_labels
 from model import calc_vhs
 
-def train_with_pseudo_labels(
-    train_loader, unlabeled_loader, device, model, optimizer, criterion, scheduler, num_epochs
-):
-    pred_record = torch.zeros([len(train_loader.dataset), 10, 12])
+def train_with_pseudo_labels(train_loader, unlabeled_loader, valid_loader, checkpoint, device, num_epochs=100, lr=1e-5):
+    model = get_model(device)
+    model.load_state_dict(checkpoint)
+    criterion = torch.nn.L1Loss()
+    optimizer = AdamW(model.parameters(), lr=lr, weight_decay=1e-6)
+    scheduler = CosineAnnealingLR(optimizer, T_max=num_epochs, eta_min=1e-6)
+    
     for epoch in range(num_epochs):
         model.train()
         running_loss = 0.0
@@ -24,10 +27,13 @@ def train_with_pseudo_labels(
             running_loss += loss.item()
             pred_record[ind, epoch % 10] = outputs.detach().cpu()
             
-        epoch_loss = running_loss / len(train_loader.dataset)
-        
         scheduler.step()
+        
+        epoch_loss = running_loss / len(train_loader.dataset)
+        val_loss, val_acc = evaluate_model(model, valid_loader, device, criterion)
 
+        print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {epoch_loss:.4f}, Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.4f}")
+        
         # Generate pseudo-labels
         high_conf_pseudo_labels, high_conf_idx = generate_pseudo_labels(model, unlabeled_loader, device)
         if len(high_conf_idx) > 0:
